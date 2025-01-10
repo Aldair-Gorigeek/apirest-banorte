ca1376c23184b67e686d70c7f3f2b43a6f5bc1f6
Aldair-Gorigeek <aldair.cabrera@gorigeek.com>
2025-01-10 11:08:36 -0600

    Delete apirest-banorte directory

--- apirest-banorte/src/main/java/com/gorigeek/banorte/apirest/dao/IBeneficiarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.gorigeek.banorte.apirest.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gorigeek.banorte.apirest.entity.Beneficiario;
-
-public interface IBeneficiarioDao extends CrudRepository<Beneficiario, Long>{
-
-}
--- apirest-banorte/src/main/java/com/gorigeek/banorte/apirest/service/IBeneficiarioService.java
+++
@@ -1,9 +0,0 @@
-package com.gorigeek.banorte.apirest.service;
-
-import java.util.List;
-
-import com.gorigeek.banorte.apirest.entity.Beneficiario;
-
-public interface IBeneficiarioService {
-	public List<Beneficiario> findAll();
-}
